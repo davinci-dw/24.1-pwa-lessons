39367458113898797bb68ff5eec4d3e528810f13
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-03-25 22:55:38 -0300

    feat: actualizar informacion de gato

--- scripts.js
+++ scripts.js
@@ -13,3 +13,15 @@ bolaDeNieve.peso = '4kg';
 
 //asignar un alias a un objeto
 const miMichi = michi; //asigné la referencia de michi a miMichi
+
+const nombreGato = document.querySelector('#gatos .nombre');
+const pesoGato = document.querySelector('#gatos .peso');
+const colorGato = document.querySelector('#gatos .color');
+const alturaGato = document.querySelector('#gatos .altura');
+
+michi.peso = '50kg';
+
+pesoGato.innerHTML = michi.peso;
+nombreGato.innerHTML = michi.nombre;
+colorGato.innerHTML = michi.color;
+alturaGato.innerHTML = michi.altura;
