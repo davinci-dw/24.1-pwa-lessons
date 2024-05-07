5ea498452fc448519bca43f1713518914198ff74
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-05-06 22:56:10 -0300

    feat: agregar intercepcion del fetch

--- clase_06/sw.js
+++ clase_06/sw.js
@@ -1,9 +1,9 @@
 const ALMACEN = 'caches';
 const LISTA_ARCHIVOS_CACHEADOS = [
-    '/',
     'michis.json',
     'styles.css',
-    'scripts.js'
+    'scripts.js',
+    '/'
 ]
 
 self.addEventListener('install', (e) => {
@@ -22,3 +22,15 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', () => {
     console.log("Soy un service worker. Y me estoy activado");
 });
+
+self.addEventListener('fetch', (e) => {
+    console.log("esta interceptando")
+    const consulta = e.request;
+    caches.match(consulta).then((respuesta) => {
+        console.log("responde", respuesta)
+        if(respuesta) return respuesta;
+        return fetch(consulta).then((respuesta) => {
+            return respuesta;
+        })
+    })
+})
