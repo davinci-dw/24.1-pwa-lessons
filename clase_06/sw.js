83d18f8f6d9afe82e43145e7493b7d2e15e52693
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-05-13 21:39:32 -0300

    fix: revolver consulta cacheada

--- clase_06/sw.js
+++ clase_06/sw.js
@@ -3,6 +3,7 @@ const LISTA_ARCHIVOS_CACHEADOS = [
     'michis.json',
     'styles.css',
     'scripts.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
     '/'
 ]
 
@@ -24,13 +25,14 @@ self.addEventListener('activate', () => {
 });
 
 self.addEventListener('fetch', (e) => {
-    console.log("esta interceptando")
+    console.log("cache only")
     const consulta = e.request;
-    caches.match(consulta).then((respuesta) => {
+    const respuestaCacheada = caches.match(consulta).then((respuesta) => {
         console.log("responde", respuesta)
         if(respuesta) return respuesta;
         return fetch(consulta).then((respuesta) => {
             return respuesta;
         })
     })
+    e.respondWith(respuestaCacheada);
 })
